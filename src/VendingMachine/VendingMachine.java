2a58e0117017c138215eda76e44020901d2bbb2b
jjtheall <jtheall@quinnipiac.edu>
2022-02-20 22:07:29 -0500

    welcome message as well as snack list implemented

--- src/VendingMachine/VendingMachine.java
+++ src/VendingMachine/VendingMachine.java
@@ -1,11 +1,21 @@
 package VendingMachine;
 
+import java.util.ArrayList;
+
 public class VendingMachine {
     //kevin make snacks here
-	Snack KitKat = new Snack("KitKat", 1.50);
+	private static Snack KitKat = new Snack("KitKat", 1.50);
+	private static ArrayList<Snack> snacks = new ArrayList<Snack>();
 
     public static void main (String[] args){
         //jack write user greeting & snack display here
+        snacks.add(KitKat);
+
+        System.out.println("Welcome to the vending machine!");
+        System.out.println("Snack options are: ");
+        for(int i=0; i<snacks.size(); i++){
+            System.out.println((i+1) + ": " + snacks.get(i).getName() + ", " + snacks.get(i).getPrice());
+        }
 
     }
 }
